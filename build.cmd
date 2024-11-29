277fd9fc6e97516296b26e25944fdecfe89047da
Shane Harper <shane@shaneharper.net>
2024-11-29 12:16:38 +1100

    Fix Windows build broken by 918e37bc0a.

--- build.cmd
+++ build.cmd
@@ -3,9 +3,9 @@ setlocal
 cd /D %~dp0
 
 if not exist %userprofile%\bin mkdir %userprofile%\bin
-cl /c /std:c++17 /EHsc /TP /FI ciso646 with_end_statements.c++ ^
- && cl /std:c++17 /EHsc /TP /FI ciso646 test.c++ /link with_end_statements.obj ^
+cl /c /std:c++20 /EHsc /TP /FI ciso646 with_end_statements.c++ ^
+ && cl /std:c++20 /EHsc /TP /FI ciso646 test.c++ /link with_end_statements.obj ^
  && test.exe ^
- && cl /std:c++17 /EHsc /TP /FI ciso646 /Fe:%userprofile%\bin\add_vim_script_end_statements.exe main.c++ /link with_end_statements.obj
+ && cl /std:c++20 /EHsc /TP /FI ciso646 /Fe:%userprofile%\bin\add_vim_script_end_statements.exe main.c++ /link with_end_statements.obj
 
 REM set PATH=%PATH%;%userprofile%\bin
